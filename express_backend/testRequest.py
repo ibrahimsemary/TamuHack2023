9d25887a235d180ad58b08dd099c1d90b6940ee5
walkerdrury <walkerdrury@tamu.edu>
2023-01-28 17:29:46 -0600

    dotenv fix

--- express_backend/testRequest.py
+++ express_backend/testRequest.py
@@ -1,7 +1,7 @@
 import requests
 
-#url = "http://localhost:4000"
-url = "https://group-sync.onrender.com"
+url = "http://localhost:4000"
+#url = "https://group-sync.onrender.com"
 a = requests.post(url + "/authenticate", json={'username': 'walkerdrury', 'password': 'password1'})
 
 print(a.text)
