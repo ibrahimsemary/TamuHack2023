7793e25b57a1f85c864865ce883c3408f3e96862
Howiemorg <morgenthalerhowie@yahoo.com>
2023-01-28 14:00:33 -0600

    beginning changes to backend

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+app.listen(3000,() => console.log("Server listening at port 3000"));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
+app.listen(3000,() => console.log("Server listening at port 3000"));
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
