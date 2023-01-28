84db53806aaf79239c0fbbb5bc117edda46e011b
Howiemorg <morgenthalerhowie@yahoo.com>
2023-01-28 17:52:32 -0600

    users can be added to groups

--- express_backend/routes/groups.js
+++ express_backend/routes/groups.js
@@ -8,7 +8,6 @@ const router = express.Router()
 
 router.post('/add-group', async(req, res) => {
     try {
-        console.log(req.body)
         const myUser = req.body.username
         const title = req.body.title
         var description = req.body.description
@@ -21,7 +20,18 @@ router.post('/add-group', async(req, res) => {
         const id = result.rows[0].max
         await client.query(`INSERT INTO groups_users (groupsid, username) 
                                 VALUES ('${id}','${myUser}')`)
-        console.log(result)
+        res.send((id).toString())
+    } catch (err) {
+        console.log(err.message);
+        res.send("User cannot be found");
+    }
+})
+router.post('/add-user-group', async(req, res) => {
+    try {
+        const myUser = req.body.username
+        const groupid = req.body.groupid
+        await client.query(`INSERT INTO groups_users (groupsid, username) 
+                                VALUES ('${groupid}','${myUser}')`)
         res.send((id).toString())
     } catch (err) {
         console.log(err.message);
