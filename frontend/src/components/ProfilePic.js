68f9a9007e3e1027112ad7b8d655527d1223d7e5
jianliu200 <jiangyuanliu2002@gmail.com>
2023-01-29 03:08:59 -0600

    Added the centering for the cards

--- frontend/src/components/ProfilePic.js
+++ frontend/src/components/ProfilePic.js
@@ -1,14 +1,5 @@
 import { Avatar } from "@mui/material";
 import React from "react";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Modal from "@mui/material/Modal";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Fade from "@mui/material/Fade";
-import InputBox from "./InputBox";
-import Header from "./Header";
-import SelectionDropdown from "./SelectionDropdown";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -27,6 +18,7 @@ const ProfilePic =(name, id)=>{
     }, []);
     return(
         <div className='avatar'>
+        
             <Avatar
                 src={imgs[name]}
                 alt='https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='
@@ -34,6 +26,7 @@ const ProfilePic =(name, id)=>{
             />
             {name}
         </div>
+        
     )
 }
 
