bcde86854e1cb80ace1e9393b22da34e98517e85
walkerdrury <walkerdrury@tamu.edu>
2023-01-29 03:29:11 -0600

    sorted schedule

--- express_backend/routes/calendar.js
+++ express_backend/routes/calendar.js
@@ -13,7 +13,7 @@ router.get('/get-schedule/:username/:date', async(req, res) =>  {
     try {
         const { username } = req.params
         const {date} = req.params
-        const result = await client.query(`SELECT * FROM event_users NATURAL JOIN events WHERE username = '${username}' AND date = '${date}' `);
+        const result = await client.query(`SELECT * FROM event_users NATURAL JOIN events WHERE username = '${username}' AND date = '${date}' ORDER BY start_time`);
         const events = []
         for (var i = 0; i < result.rows.length; ++i){
             events[i] = new Array(4)
@@ -25,6 +25,18 @@ router.get('/get-schedule/:username/:date', async(req, res) =>  {
             events[i][5] = result.rows[i].title;
         }
         res.send(events)
+    } catch (err) {
+        console.log(err.message);
+        res.send(err.message);
+    }
+});
+
+router.get('/get-group-schedule/:groupsid/:date', async(req, res) =>  {
+    try {
+        const {groupsid, date} = req.params
+        await client.query(`CREATE OR REPLACE VIEW view11 AS SELECT event_users.username, eventid FROM groups_users JOIN event_users ON groups_users.username = event_users.username WHERE groupsid='${groupsid}'`)
+        const result = await client.query(`SELECT username, start_time, end_time FROM view11 JOIN events ON view11.eventid = events.eventid`)
+        res.send(result.rows);
     } catch (err) {
         console.log(err.message);
         res.send(err.message);
